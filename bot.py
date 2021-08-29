7df712423759695b726096a073af3e823580f8c1
IET Updates <83505196+IETUpdates@users.noreply.github.com>
2021-08-29 15:18:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,10 +93,10 @@ async def _(event):
         x = await get_user_join(user.id)
         if x is True:
             msg = welcome_msg.format(mention=mention, title=title, fullname=fullname, username=username, name=name, last=last, channel=f"@{channel}")
-            butt = [Button.url("Channel", url=f"https://t.me/{channel}")]
+            butt = [Button.url("🍁 𝘾𝙃𝘼𝙉𝙉𝙀𝙇", url=f"https://t.me/{channel}")]
         else:
             msg = welcome_not_joined.format(mention=mention, title=title, fullname=fullname, username=username, name=name, last=last, channel=f"@{channel}")
-            butt = [Button.url("Channel", url=f"https://t.me/{channel}"), Button.inline("UnMute Me", data=f"unmute_{user.id}")]
+            butt = [Button.url("🍁 𝘾𝙃𝘼𝙉𝙉𝙀𝙇", url=f"https://t.me/{channel}"), Button.inline("😐 UnMute Me", data=f"unmute_{user.id}")]
             await BotzHub.edit_permissions(event.chat.id, user.id, until_date=None, send_messages=False)
         
         await event.reply(msg, buttons=butt)
@@ -119,7 +119,7 @@ async def mute_on_msg(event):
         except Exception as e:
             print(str(e))
             return
-        await event.reply(f"Hey {nm}, seems like you haven't joined our channel. Please join @{channel} and then press the button below to unmute yourself!", buttons=[[Button.url("Channel", url=f"https://t.me/{channel}")], [Button.inline("UnMute Me", data=f"unmute_{event.sender_id}")]])
+        await event.reply(f"Hey {nm}, seems like you haven't joined our channel. Please join @{channel} and then press the button below to unmute yourself!\n\nതാഴെ ഉള്ള ലിങ്ക് ഇൽ ക്ലിക്ക് ചെയ്ത് ചാനലിൽ ജോയിൻ ചെയ്ത ശേഷം unmute me എന്ന ബട്ടൺ ക്ലിക്ക് ചെയ്യുക..", buttons=[[Button.url("🍁𝘾𝙃𝘼𝙉𝙉𝙀𝙇🍁", url=f"https://t.me/{channel}")], [Button.inline("😐 UnMute Me", data=f"unmute_{event.sender_id}")]])
 
 
 @BotzHub.on(events.callbackquery.CallbackQuery(data=re.compile(b"unmute_(.*)")))
@@ -137,15 +137,15 @@ async def _(event):
                 print(str(e))
                 return
             msg = f"Welcome to {(await event.get_chat()).title}, {nm}!\nGood to see you here!"
-            butt = [Button.url("Channel", url=f"https://t.me/{channel}")]
+            butt = [Button.url("𝘾𝙃𝘼𝙉𝙉𝙀𝙇", url=f"https://t.me/{channel}")]
             await event.edit(msg, buttons=butt)
     else:
         await event.answer("You are an old member and can speak freely! This isn't for you!", cache_time=0, alert=True)
 
 @BotzHub.on(events.NewMessage(pattern="/start"))
 async def strt(event):
-    await event.reply(f"Hi. I'm a force subscribe bot made specially for @{channel}!\n\nCheckout @BotzHub :)", buttons=[Button.url("Channel", url=f"https://t.me/{channel}"), Button.url("Repository", url="https://github.com/xditya/ForceSub")])
+    await event.reply(f"Hi. I'm a force subscribe bot made specially for @{channel}!\n\nCheckout @IET_Owner :)", buttons=[Button.url("🌻 𝘾𝙃𝘼𝙉𝙉𝙀𝙇", url=f"https://t.me/{channel}"), Button.url("👩‍💻 Developer", url="https://t.me/iet_owner")])
 
     
-print("ForceSub Bot has started.\nDo visit @BotzHub!")
+print("ForceSub Bot has started.\nDo visit @IET_Updates!")
 BotzHub.run_until_disconnected()
